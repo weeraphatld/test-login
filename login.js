4b7873358461abf414e357a7d02c41ac90597243
weeraphatld <155152628+weeraphatld@users.noreply.github.com>
2024-03-28 08:05:52 +0700

    Update login.js

--- login.js
+++ login.js
@@ -5,24 +5,30 @@ function login() {
     // ลิงค์ไปยัง API ของ Sheety
     var api_url = 'https://api.sheety.co/f42cd6efeb33849230069d25276e089e/loginWeeraphatWebsite/sheet1';
 
-    fetch(api_url)
-        .then(response => response.json())
-        .then(data => {
-            var loginSuccess = false;
-            data.sheet1.forEach(function(user) {
-                if (user.username == username && user.password == password) {
-                    loginSuccess = true;
-                }
-            });
+    var headers = new Headers();
+    headers.append("Authorization", "Basic bmFuZ25nbmdfbmc6d2VlcmFwaGF0bmFuZ3N1ZTM2NTk=");
 
-            if (loginSuccess) {
-                document.getElementById('loginResult').textContent = 'Login successful!';
-            } else {
-                document.getElementById('loginResult').textContent = 'Invalid username or password!';
+    fetch(api_url, {
+        method: "GET",
+        headers: headers
+    })
+    .then(response => response.json())
+    .then(data => {
+        var loginSuccess = false;
+        data.sheet1.forEach(function(user) {
+            if (user.username == username && user.password == password) {
+                loginSuccess = true;
             }
-        })
-        .catch(error => {
-            console.error('Error:', error);
-            document.getElementById('loginResult').textContent = 'An error occurred!';
         });
+
+        if (loginSuccess) {
+            document.getElementById('loginResult').textContent = 'Login successful!';
+        } else {
+            document.getElementById('loginResult').textContent = 'Invalid username or password!';
+        }
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        document.getElementById('loginResult').textContent = 'An error occurred!';
+    });
 }
